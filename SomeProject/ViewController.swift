dd8f71cd04267e759722cf1037eb424dd2feb490
demothreen <demonnn@list.ru>
2019-01-13 19:17:53 +0500

    Initial Commit

---
+++ SomeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SomeProject
+//
+//  Created by demothreen on 13/01/2019.
+//  Copyright © 2019 demothreen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
