54748c2d8c58dc6534a8f0f7f9c428b6a5ce68e2
demothreen <demonnn@list.ru>
2019-01-14 19:59:35 +0500

    init

--- SomeProject/ViewController.swift
+++ SomeProject/ViewController.swift
@@ -8,11 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Photos: Codable {
+    let albumId: Int?
+    let id: Int?
+    let title: String?
+    let url: String?
+    let thumbnailUrl: String?
+}
 
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var imageItems: UICollectionView!
+    
+    var photos = [Photos]()
+    let url = URL(string: "https://jsonplaceholder.typicode.com/photos")!
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = imageItems.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as UICollectionViewCell
+        
+        return cell
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imageItems.delegate = self
+        imageItems.dataSource = self
+        
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else { return }
+            do{
+                let decoder = JSONDecoder()
+                self.photos = try decoder.decode([Photos].self, from: data)
+                for photo in self.photos{
+                    print(photo.url!)
+                }
+            }catch let err{
+                print(err)
+            }
+//            print(String(data: data, encoding: .utf8)!)
+        }
+        task.resume()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 
